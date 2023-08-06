bb77d16d4f386d08e8c24252fda2f6657c0c1650
SanaS7 <126742643+SanaS7@users.noreply.github.com>
2023-08-06 18:35:07 +0500

    changing productlist

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -1,54 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import { useSelector } from 'react-redux';
+
+
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
-  const { currentCategory } = state;
+  const { products } = useSelector((state) => state.products);
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  useEffect(() => {
-    if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-    } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
-      });
-    }
-  }, [data, loading, dispatch]);
 
-  function filterProducts() {
-    if (!currentCategory) {
-      return state.products;
-    }
-
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
-    );
-  }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {products?.length ? (
         <div className="flex-row">
-          {filterProducts().map((product) => (
+          {products?.map((product) => (
             <ProductItem
               key={product._id}
               _id={product._id}
